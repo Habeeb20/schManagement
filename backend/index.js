5883b301275bba1e722d391d403d02b0eea8448b
Habeeb <ademolawaliyu1@gmail.com>
2024-08-08 09:54:06 +0100

    fixing erros

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`)
 })
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
 })
